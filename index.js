d713347766f3202889df7c760e1b76347053bd0e
Maulana <maulanansn@gmail.com>
2023-10-01 15:34:44 +0700

    Add Morgan in index.js

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express()
 const PORT = 8082 || process.env.PORT
+const morgan = require('morgan')
+
+app.use(morgan('tiny'))
 
 app.get('/', (request, response) => {
     response.json({message: 'Ping Succesfully'})
@@ -11,5 +14,17 @@ app.listen(PORT, () => {
 })
 
 app.get('/produk', (request, response) => {
-    response.json({data: [{id: 1, name: 'Ultra Milk'}]})
+    response.json({data: [{ id: 1, name: 'Ultra Milk'}]})
+})
+
+app.post('/produk/', (request, response) => {
+    response.send("Berhasil menambahkan data baru.")
+
+app.put('/produk/:id', (request,response ) => {
+    response.send("Berhasil mengubah data.")
+})
+})
+
+app.delete('/produk/:id', (request, response) => {
+    response.send("Berhasil menghapus data.")
 })
